e4849eb2f75ce0149bcef811003de865c228a1b8
Natron Kan <waterbeside@163.com>
2023-02-05 10:05:04 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,4 @@
-import { defineUserConfig } from '@vuepress/cli'
+import { defineUserConfig } from 'vuepress'
 import theme from './theme.js'
 // import { searchProPlugin } from "vuepress-plugin-search-pro";
 // import { redirectPlugin } from "vuepress-plugin-redirect";
